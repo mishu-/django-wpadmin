92323886099a46f60cf0d2ad558aacac348ed4f3
HUN73R <alexandru.leca89@gmail.com>
2016-08-05 18:08:56 +0300

    Fix: testing

--- wpadmin/menu/items.py
+++ wpadmin/menu/items.py
@@ -114,8 +114,13 @@ class AppList(AppListElementMixin, MenuItem):
         for model, perms in items:
             if not perms['change'] and not perms['add']:
                 continue
-            import pdb; pdb.set_trace()
-            app_label = model._meta.app_config.verbose_name
+            try:
+                app_label = model._meta.app_config.verbose_name
+            except:
+                try:
+                    app_label = model._meta.model_name
+                catch:
+                    app_label = ''
             if app_label not in apps:
                 apps[app_label] = {
                     'title': capfirst(_(app_label.title())),
